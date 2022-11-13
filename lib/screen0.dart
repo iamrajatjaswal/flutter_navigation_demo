62acee039065c5e1be378a07393e8393d87cebd6
Rajat Jaswal <iamrajatjaswal@gmail.com>
2022-11-13 23:23:02 +0530

    navigation demo tutorial completed

---
+++ lib/screen0.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Screen0 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text('Screen 0'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+              ),
+              // color: Colors.red,
+              child: Text('Go To Screen 1'),
+              onPressed: () {
+                //Navigate to Screen 1
+                Navigator.pushNamed(context, '/first');
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+              ),
+              // color: Colors.blue,
+              child: Text('Go To Screen 2'),
+              onPressed: () {
+                //Navigate to Screen 2
+                Navigator.pushNamed(context, '/second');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
